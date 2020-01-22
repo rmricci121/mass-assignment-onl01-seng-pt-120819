4248db350f1c289dedad165f76e039616875f484
Robert M Ricci <rmricci121@gmail.com>
2020-01-22 10:49:57 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,5 +5,5 @@ class Person
 :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(attributes)
-    attributes.each {|key, value| self.send (("#{key}= , valur)
+    attributes.each {|key, value| self.send (("#{key}=) , value)
 end
